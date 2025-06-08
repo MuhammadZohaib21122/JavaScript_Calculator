0f0e419e2427484478858eee78f859b6ea5ef1ce
MUHAMMAD ZOHAIB <muhammadzohaib21122@gmail.com>
2025-06-08 12:44:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,31 +8,28 @@ const operators = {            // mathematical operator operation on two numbers
     '/': (a, b) => a / b
 };
 
-
 function appendToDisplay(input) {        //add numbers or operators to the display.
 
     const secondLastChar = display.value.charAt(display.value.length - 1);
-        
-    console.error(typeof(lastChar));
-    if ((secondLastChar === '+' || secondLastChar === '-' || secondLastChar === '*' || secondLastChar === '/' || secondLastChar === '.' ) && (input === '+' || input === '-' || input === '*' || input === '/' || input === '.' )) {
+
+    console.error(typeof (lastChar));
+    if ((secondLastChar === '+' || secondLastChar === '-' || secondLastChar === '*' || secondLastChar === '/' || secondLastChar === '.') && (input === '+' || input === '-' || input === '*' || input === '/' || input === '.')) {
 
         display.value = display.value.slice(0, -1);
         display.value += input;
-        
+
     }
-    else
-    {
+    else {
         display.value += input;
     }
-    
+
 }
 
-
-function cleardisplay(){  // they can empty the input value
-display.value = "";
+function cleardisplay() {  // they can empty the input value
+    display.value = "";
 }
 
-function calculate() { 
+function calculate() {
 
     const input = display.value.trim();          // they gain value to  variable input
     let operator;
@@ -47,18 +44,18 @@ function calculate() {
     }
 
     if (operands && operands.length === 2) {   // they are check if they are two element ,they perform calculate
-        const a = parseFloat(operands[0].trim()); 
+        const a = parseFloat(operands[0].trim());
         const b = parseFloat(operands[1].trim());  // These lines converting the two operands from strings to  numbers
 
         if (!isNaN(a) && !isNaN(b)) {            //This checks if both a and b are valid numbers
             const result = operators[operator](a, b); //the result is stored in the variable result.
             display.value = result; //The result of the calculation is displayed in the display element.
-           
-        }else{
-            display.value ="Error"; //If two or one, a or b is not a valid number, the display is set to "Error"
+
+        } else {
+            display.value = "Error"; //If two or one, a or b is not a valid number, the display is set to "Error"
         }
-    }else{
-        display.value ="Error";
+    } else {
+        display.value = "Error";
     }
-    
+
 }
